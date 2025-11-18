5dcc8cdfe33b8a1edcaecb84078630f37e26c06a
amelnikov@noosoft.ru <amelnikov@noosoft.ru>
2025-11-18 20:02:18 +0300

    add: data module

---
+++ data/src/main/java/com/example/data/bluetooth/BluetoothManager.kt
@@ -0,0 +1,8 @@
+package com.example.data.bluetooth
+import android.bluetooth.BluetoothManager
+import androidx.core.content.ContextCompat.getSystemService
+
+
+class BluetoothManager {
+
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,3 +21,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "BluetoothProject"
 include(":app")
+include(":data")
